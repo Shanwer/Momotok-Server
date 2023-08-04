14f2c3918cbef26c7ef45f5d0f849a20d491ef46
Yana-Hoshino <qq1512821889@outlook.com>
2023-08-05 00:20:54 +0800

    feed Update v1.5 Fix the restart page problem

--- controller/feed.go
+++ controller/feed.go
@@ -15,36 +15,38 @@ type FeedResponse struct {
 	NextTime  int64   `json:"next_time,omitempty"`
 }
 
+var currentPage = 1 //全局变量记录当前page
+
 // Feed same demo video list for every request
 func Feed(c *gin.Context) {
-	videos := makeVideoList() //调用方法返回视频列表
+	perPage := 30                                 // 默认每页加载 30 个视频
+	videos := makeVideoList(currentPage, perPage) //调用方法返回视频列表
 	c.JSON(http.StatusOK, FeedResponse{
 		Response:  Response{StatusCode: 0},
 		VideoList: videos,
 		NextTime:  time.Now().Unix(),
 	})
+	currentPage++
 }
 
-var currentPage = 1 //全局变量记录当前page
-
-func makeVideoList() []Video {
+func makeVideoList(page, perPage int) []Video {
 	db, err := sql.Open("mysql", DatabaseAddress) //连接数据库
 	//数据库表名为video，字段为id, author_id, play_url, cover_url, favorite_count, comment_count, is_favorite, title，&publish_time具体类型见下述定义
 	if err != nil {
 		fmt.Println("Failed to connect to database:", err)
 	}
-	defer db.Close()
-	perPage := 30                                                                                             //设置最大加载数
-	page := currentPage                                                                                       //设置页数
-	offSet := (page - 1) * perPage                                                                            //offSet:视频开始位置
+	defer db.Close() //设置页数
+
+	offSet := (page - 1) * perPage //offSet:视频开始位置
+
 	rows, err := db.Query("SELECT * FROM video ORDER BY publish_time DESC LIMIT ? OFFSET ?", perPage, offSet) //写入sql指令，按倒序查找列                                                                           //执行上述指令
 	if err != nil {
 		fmt.Println("Failed to execute query:", err)
 		return nil
 	}
 	defer rows.Close()
-
 	videos := make([]Video, 0) //创建视频列表
+	isLast := 0
 	for rows.Next() {
 		//循环读取直到列结束
 		var id int64
@@ -77,7 +79,10 @@ func makeVideoList() []Video {
 		}
 		videos = append(videos, video) //视频切片加入视频列表
 		fmt.Println(videos)
+		islast++
 	}
-	currentPage++
+	if isLast < perPage {
+		currentPage = 0
+	}
 	return videos //返回视频列表
 }
