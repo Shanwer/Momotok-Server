7556e8f63f13a52ea30d5eff6f3a9e20c3fc21dd
clt123haha <121712843+clt123haha@users.noreply.github.com>
2023-08-11 00:31:30 +0800

    Add files via upload

--- controller/relation.go
+++ controller/relation.go
@@ -1,6 +1,10 @@
 package controller
 
 import (
+	"Momotok-Server/system"
+	"Momotok-Server/utils"
+	"database/sql"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -13,12 +17,75 @@ type UserListResponse struct {
 // RelationAction no practical effect, just check if token is valid
 func RelationAction(c *gin.Context) {
 	token := c.Query("token")
+	to_user_id := c.Query("to_user_id")
+	action_type := c.Query("action_type")
+	id := -1
 
-	if _, exist := usersLoginInfo[token]; exist {
-		c.JSON(http.StatusOK, Response{StatusCode: 0})
-	} else {
-		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
+	if action_type == "" || to_user_id == "" {
+		c.JSON(http.StatusOK, Response{
+			StatusCode: 1,
+			StatusMsg:  "Incomplete information",
+		})
+		return
 	}
+
+	uid, err := utils.GetUID(token)
+	if err != nil {
+		c.JSON(http.StatusOK, Response{
+			StatusCode: 1,
+			StatusMsg:  "invalid token",
+		})
+		return
+	}
+
+	db, err := sql.Open("mysql", system.ServerInfo.Server.DatabaseAddress)
+	if err != nil {
+		fmt.Println("Database connected failed: ", err)
+	}
+
+	if action_type == "1-关注" {
+		_ = db.QueryRow("select  id from concern where `concern_uid` = ? AND `concern_at_uid` = ?", uid, to_user_id).Scan(&id)
+		if id != -1 {
+			c.JSON(http.StatusOK, Response{
+				StatusCode: 1,
+				StatusMsg:  "The condition is not true",
+			})
+			return
+		}
+
+		_, err = db.Exec("INSERT INTO concern (concern_uid, concern_at_uid) VALUES (?, ?)", uid, to_user_id)
+		if err != nil {
+			c.JSON(http.StatusOK, Response{
+				StatusCode: 1,
+				StatusMsg:  "Concern failed",
+			})
+			return
+		}
+	} else if action_type == "2-取消关注" {
+		_ = db.QueryRow("select  id from concern where `concern_uid` = ? AND `concern_at_uid` = ?", uid, to_user_id).Scan(&id)
+		if id == -1 {
+			c.JSON(http.StatusOK, Response{
+				StatusCode: 1,
+				StatusMsg:  "The condition is not true",
+			})
+			return
+		}
+
+		_, err = db.Exec("DELETE FROM `momotok`.`concern` WHERE`concern_uid` = ? AND `concern_at_uid` = ?", uid, to_user_id)
+		if err != nil {
+			c.JSON(http.StatusOK, Response{
+				StatusCode: 1,
+				StatusMsg:  "Unfollow failed",
+			})
+			return
+		}
+	}
+
+	c.JSON(http.StatusOK, Response{
+		StatusCode: 0,
+		StatusMsg:  "success",
+	})
+	return
 }
 
 // FollowList all users have same follow list
